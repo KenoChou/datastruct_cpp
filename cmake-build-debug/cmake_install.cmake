48c9dd3c03f148342afae57e60af411256ef2fab
kenochou <2078661854@qq.com>
2022-10-12 17:56:36 +0800

    test

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kenochou/CLionProjects/untitled2
+# Install script for directory: /home/kenochou/CLionProjects/datastruct_cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kenochou/CLionProjects/untitled2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/kenochou/CLionProjects/datastruct_cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     InitStack(S);
     Push(S,4);
 Push(S,3);
-    Pop(S,0);
+    //Pop(S,0);
    std::cout<<S.elem[1]<<std::endl;
     //std::cout << "Hello, World!" << std::endl;
     return 0;
