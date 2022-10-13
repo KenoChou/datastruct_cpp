87e6a9274b709cd7dce8e8fdf26f26af5c373311
kenochou <2078661854@qq.com>
2022-10-13 14:12:01 +0800

    14_11

---
+++ tree/BinTree.h
@@ -0,0 +1,14 @@
+//
+// Created by kenochou on 22-10-13.
+//
+
+#ifndef DATASTRUCT_CPP_BINTREE_H
+#define DATASTRUCT_CPP_BINTREE_H
+typedef char TElemType;
+#define  maxSzie 10
+typedef struct {
+    TElemType data[maxSzie];
+    int n;
+}SeqBtree;
+
+#endif //DATASTRUCT_CPP_BINTREE_H
