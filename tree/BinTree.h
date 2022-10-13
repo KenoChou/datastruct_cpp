ee19f8561429a83cd8a2286f912b6a368fd481f8
kenochou <2078661854@qq.com>
2022-10-13 14:44:14 +0800

    14_31

--- tree/BinTree.h
+++ tree/BinTree.h
@@ -4,11 +4,41 @@
 
 #ifndef DATASTRUCT_CPP_BINTREE_H
 #define DATASTRUCT_CPP_BINTREE_H
+
+#include <cstdio>
+
 typedef char TElemType;
 #define  maxSzie 10
 typedef struct {
     TElemType data[maxSzie];
     int n;
 }SeqBtree;
-
+typedef char TelemType;
+typedef  struct node{
+    TElemType data;
+    struct  node* lchild,*rchild;
+}BiTnode,*BinTree;
+void InOrder_recur(BiTnode * BT)
+{
+    if(BT != NULL){
+        InOrder_recur(BT->lchild);
+        printf("%d",BT->data);
+        InOrder_recur(BT->rchild);
+    }
+}
+void PreOrder_recur(BiTnode* BT){
+    if(BT!=NULL){
+        printf("%d",BT->data);
+        PreOrder_recur(BT->lchild);
+        PreOrder_recur(BT->rchild);
+    }
+}
+void PostOrder_recur(BiTnode * BT){
+    if(BT!=NULL){
+        PostOrder_recur(BT->lchild);
+        PreOrder_recur(BT->rchild);
+        printf("%d",BT->data);
+    }
+}
 #endif //DATASTRUCT_CPP_BINTREE_H
+
