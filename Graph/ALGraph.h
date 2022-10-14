bcaf791d23c7ae2e5d2831f7efde707d2e602692
kenochou <2078661854@qq.com>
2022-10-14 16:55:27 +0800

    22-10-14-17——00

--- Graph/ALGraph.h
+++ Graph/ALGraph.h
@@ -62,5 +62,37 @@ Weight getWeight(ALGraph& G,int v,int w){
     if(p!=NULL)return p->cost;
     else return impossibleWeight;
 }
-
+void createALGraph(ALGraph& G,int n,int e,int d){
+    int i,j,k;Type val,e1,e2;Weight c;EdgeNode *q,*p;
+    G.numVertices=n;G.numEdges=e;
+    for(i=0;i<G.numVertices;i++){
+        scanf("%c",&val);
+        G.VerticesList[i].data=val;
+        G.VerticesList[i].adj=NULL;
+    }
+    i=0;
+    while (i<G.numEdges){
+        scanf("%c,%c,%d",&e1,&e2,&c);
+      j= getVertexPos(G,e1);k= getVertexPos(G,e2);
+      if(j!=-1&&k !=-1){
+          p=G.VerticesList[j].adj;
+          while (p!=NULL&&p->dest !=k)p=p->link;
+          if(p==NULL){
+              q=(EdgeNode*) malloc(sizeof(EdgeNode));
+              q->dest=k;q->cost=c;
+              q->link=G.VerticesList[j].adj;
+              G.VerticesList[j].adj=q;
+              if(d==0){
+                  q=(EdgeNode*) malloc(sizeof (EdgeNode));
+                  q->dest=j;q->cost=c;
+                  q->link=G.VerticesList[k].adj;
+                  G.VerticesList[k].adj=q;
+              }
+              i++;
+          }
+          else printf("边重复，重新输入！\n");
+      }
+      else printf("边两端顶点信息有误，重新输入！\n");
+    }
+}
 #endif //DATASTRUCT_CPP_ALGRAPH_H
